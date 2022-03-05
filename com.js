5dd37295be3780f82a4fff8a206fd799d2d16af5
McbeEringi <46889548+McbeEringi@users.noreply.github.com>
2022-03-05 16:06:56 +0900

    new_UI

--- com.js
+++ com.js
@@ -1,6 +1,6 @@
 document.body.insertAdjacentHTML('afterbegin',`<pre id="log"></pre>`);
 const llog=(...x)=>log.textContent+=`${x.join(', ')}\n`,
-table=x=>'<table><tr>'+x.map(y=>y.map((z,i)=>`<t${'hd'[i]}>${z}</t${'hd'[i]}>`).join('')).join('</tr><tr>')+'</tr></table>',
+table=(...x)=>'<table><tr>'+x.map(y=>y.map((z,i)=>`<t${'h'[i]||'d'}>${z}</t${'h'[i]||'d'}>`).join('')).join('</tr><tr>')+'</tr></table>',
 urlq=Object.fromEntries(location.search.substr(1).split('&').filter(y=>y).map(x=>x.split('=',2).map(y=>y.split('+')))),
 urlq_=x=>Object.entries({...urlq,...x}).map(y=>y[0]+'='+y[1].join('+')).join('&'),
 n2d=x=>{if(!x)return;x=`20${x}`;return[x.slice(0,4),x.slice(4,6),x.slice(6,8)];};

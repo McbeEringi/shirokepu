372495c64b303e7b0afbe91ce7fda57b92ff87a3
McbeEringi <46889548+McbeEringi@users.noreply.github.com>
2022-03-07 17:16:40 +0900

    -

--- bg.js
+++ bg.js
@@ -1,25 +1,46 @@
-document.body.insertAdjacentHTML('afterbegin',`<canvas id="bgc"></canvas>`);
-Promise.all(
-	['img/ico/m0.png','img/ico/m1.png','img/ico/m2.png','img/ico/m3.png','img/ico/i0.png','img/ico/i1.png','img/ico/i2.png','img/ico/i3.png']
-	.map(x=>new Promise(f=>x=Object.assign(new Image(),{src:x,onload:()=>f(x)})))
-).then(img=>{
-	console.log(img);
-	const w=()=>document.documentElement.clientWidth,
-		h=()=>document.documentElement.clientHeight
-		ctx=bgc.getContext('2d'),
-		draw=()=>{
-			ctx.clearRect(0,0,bgc.width,bgc.height);
-			const size=[Math.ceil(w()/256),Math.ceil(h()/256)],
-				rand=new Uint8Array(size[0]*size[1]);
-			crypto.getRandomValues(rand);console.log(rand);
-			new Array(size[0]).fill().map((_,i)=>
-				new Array(size[1]).fill().map((_,j)=>{
-					ctx.save();
-					ctx.beginPath();ctx.arc(64+i*256,64+j*256,32,0,2*Math.PI);ctx.clip();
-					ctx.drawImage(img[Math.floor(img.length*rand[i*size[1]+j]/256)],32+i*256,32+j*256,64,64);
-					ctx.restore();
-				})
-			);
-		};
-	(onresize=()=>{bgc.width=w()*devicePixelRatio;bgc.height=h()*devicePixelRatio;bgc.style.width=w()+'px';bgc.style.height=h()+'px';ctx.scale(devicePixelRatio,devicePixelRatio);draw();})();
-});
+'use strict'; 
+document.body.insertAdjacentHTML('afterbegin',`<canvas id="c" style="opacity:0;"></canvas>`);
+Promise.all([
+	fetch('https://cdn.jsdelivr.net/gh/liabru/matter-js/build/matter.min.js').then(x=>x.text()),
+	fetch('https://cdn.jsdelivr.net/gh/liabru/matter-wrap/build/matter-wrap.min.js').then(x=>x.text()),
+	//fetch('https://mcbeeringi.github.io/src/matter/floatpr.js').then(x=>x.text()),
+	fetch('https://mcbeeringi.github.io/src/matter/spriter.js').then(x=>x.text())
+]).then(x=>{
+	Function(x.join('\n'))();
+	Matter.use(MatterWrap);
+	//Matter.use(FloatPR);
+	Matter.use(Spriter);
+	const engine=Matter.Engine.create({gravity:{scale:0}}),
+		render=Matter.Render.create({
+			canvas:c,
+			engine,
+			options:{
+				width:document.documentElement.clientWidth,height:document.documentElement.clientHeight,
+				pixelRatio:'auto',//showMousePosition:true,
+				wireframes:false,background:'#0000'
+			}
+		}),
+		stack=Matter.Composites.stack(render.options.width/2-235,render.options.height/2-355,8,8,10,10,(x,y)=>{
+			const s=Matter.Common.random(10,40);
+			return Matter.Bodies.circle(x,y,s,{
+				frictionAir:.001,restitution:.5,
+				render:{sprite:{texture:`img/ico/${['m0','m1','m2','m3','i0','i1','i2','i3'][Math.floor(((s*10)%1)*8)]}.png`,xScale:s/128,yScale:s/128}},
+				plugin:{wrap:{min:{x:render.bounds.min.x,y:render.bounds.min.y},max:{x:render.bounds.max.x,y:render.bounds.max.y}}}
+			});
+		}),
+		//mouse=Matter.Mouse.create(render.canvas),
+		//mc=Matter.MouseConstraint.create(engine,{mouse,constraint:{render:{visible:false}}}),
+		runner=Matter.Runner.create(),
+		vrand=(_,r=Math.random(),t=r*20*Math.PI)=>Matter.Body.setVelocity(stack.bodies[Math.floor(stack.bodies.length*r)],Matter.Vector.create(Math.cos(t)*10,Math.sin(t)*10));
+
+	//render.mouse=mouse;
+	Spriter.stdMod=(ctx,img)=>{ctx.beginPath();ctx.ellipse(img.width/2,img.height/2,img.width/2,img.height/2,0,0, 2*Math.PI);ctx.clip();ctx.drawImage(img,0,0);};
+	Matter.Composite.add(engine.world,[
+		stack,
+		//mc
+	]);
+	Matter.Render.run(render);
+	Matter.Runner.run(runner,engine);
+	c.style.opacity='';
+	vrand();setInterval(vrand,2000);
+})
